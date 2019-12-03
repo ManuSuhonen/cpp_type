06266406036b3b68beb84b7ccdbad87a61955f13
johnzena <manu.suhonen@gmail.com>
2019-12-03 22:07:55 +0200

    Create typeid.cpp
    returns type information as string

---
+++ typeid.cpp
@@ -0,0 +1,11 @@
+namespace type
+{
+    template<typename T>
+    string get_type(T&& t)
+    {
+        std::size_t len = 0 ;
+        int status = 0 ;
+        std::unique_ptr< char, decltype(&std::free) > ptr(__cxxabiv1::__cxa_demangle( typeid(T).name(), nullptr, &len, &status ), &std::free ) ;
+        return ptr.get() ;
+    }
+}
